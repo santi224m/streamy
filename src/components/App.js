41451a36fa93f3253b95a68d73067f7ecbe14990
Santiago Mondragon <santiago.mondragon224@gmail.com>
2021-02-19 14:12:37 -0800

    project is now working

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Router, Route, Link, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/edit" component={StreamEdit} />
-                    <Route path="/streams/delete" component={StreamDelete} />
-                    <Route path="/streams/show" component={StreamShow} />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 }
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../actions';
+import { Link } from 'react-router-dom';
 
-const StreamDelete = () => {
-    return <div>StreamDelete</div>
-};
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        console.log(this.props)
+        console.log(this.props.match.params.id)
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-export default StreamDelete;
+    renderActions() {
+        const id = this.props.match.params.id;
+
+        return (
+        <React.Fragment>
+            <button onClick={() => this.props.deleteStream(id)} className="ui negative button">Delete</button>
+            <Link to="/" className="ui button">Cancel</Link>
+        </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if(!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete this stream with title: ${this.props.stream.title}`
+    }
+
+    render() {
+        return (
+            <Modal
+                title="Delete Stream"
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
